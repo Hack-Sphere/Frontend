b1d99310b655d948614105b3407cc54d33cad038
Hack-Sphere <hackspherelive@gmail.com>
2025-06-23 17:43:33 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import {visualizer} from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
-    // base: '/Hacksphere/', // e.g., '/Hacksphere/'
+    base: '/Frontend/', // e.g., '/Hacksphere/'
     plugins: [
         visualizer({ open: true }),
         tailwindcss(),
